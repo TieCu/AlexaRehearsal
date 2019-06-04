a2d86c5719f5d6fed479941d9514aa6f18432c95
Melanie <timdog563@gmail.com>
2019-06-03 19:18:52 -0600

    Added my 2 functions in the code

--- Melanie/index.js
+++ Melanie/index.js
@@ -42,7 +42,7 @@ const RandomRollHandler = {
         var allRolls = [];
         var totalCount = 0;
         for (let index = 0; index < variableRolls; index++) {
-            var roll = Math.floor((Math.random() + 1) * dieType);
+            var roll = Math.floor((Math.random() * dieType + 1));
             allRolls.push(roll);
             totalCount += roll;
         }
@@ -79,17 +79,142 @@ const StatRollHandler = {
         switch (rollType) {
             case '3d6':
                 for (let index = 0; index < 3; index++) {
-                    result += Math.floor()
-                    
+                    result += Math.floor(6 * Math.random() + 1);
                 }
                 break;
-        
-            default:
+            case '4d6 drop low':
+                var rolls = [];
+                for (let index = 0; index < 4; index++) {
+                    rolls.push(Math.floor(6 * Math.random() + 1));
+                }
+                var lowest = 10;
+                for (let index = 0; index < rolls.length; index++) {
+                    const element = rolls[index];
+                    if(element < lowest){
+                        lowest = element;
+                    }
+                }
+                rolls.forEach(element => {
+                   result += element
+                });
+                rolls -= lowest;
+                break;
+            case '3d6 reroll 1\'s':
+                for(let index = 0; index < 3; index++){
+                    var roll;
+                    do{
+                        roll = Math.floor(6 * Math.random() + 1);
+                    }while(roll === 1);
+                    results += roll;
+                }
                 break;
         }
+        var outputText = "Your stat roll was " + result;
+        return handlerInput.responseBuilder.speak(outputText).getResponse();
     }
 }
+const ComplementaryColorHandler = {
+    canHandle(handlerInput){
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+        && handlerInput.requestEnvelope.request.intent.name === 'StatRollIntent'
+    },
+    handle(handlerInput){
+        var r = Math.min(255, handlerInput.requestEnvelope.request.intent.slots.Red);
+        var g = Math.min(255, handlerInput.requestEnvelope.request.intent.slots.Green);
+        var b = Math.min(255, handlerInput.requestEnvelope.request.intent.slots.Blue);
 
+        var R = r/255;
+        var G = g/255;
+        var B = b/255;
+
+        var Cmax = Math.max(R, G, B);
+        var Cmin = Math.min(R, G, B);
+        var delta = Cmax - Cmin;
+
+        var H;
+        var S;
+        var V;
+
+        //Calculating the Hue value for the color that was just passed
+        switch(Cmax){
+            case Cmax === 0:
+                H = 0;
+                break;
+            case Cmax === R:
+                H = 60 * (0 + (G - B)/(Cmax - Cmin));
+                break;
+            case Cmax === G:
+                H = 60 * (2 + (B - R)/(Cmax - Cmin));
+                break;
+            case Cmax === B:
+                H = 60 * (4 + (R - G)/(Cmax - Cmin));
+                break;
+        }
+
+        //Calculating the Saturation value for the passed color
+        if(Cmax === 0 || (R === G && G === B)){
+            S = 0;
+        }
+        else{
+            S = (Cmax - Cmin)/Cmax;
+        }
+
+        //"Calculating" the Value for the passed color
+        V = Cmax;
+
+        //Calculating the complementary color
+        H = ((H + 180) % 360);
+        
+        //Converting Back
+        var Chroma = V * S;
+        var Hprime = H/60;
+        var X = Chroma * (1 - Math.abs(Hprime % 2 - 1));
+
+        var R1;
+        var G1;
+        var B1;
+
+        switch(Hprime){
+            case 0 <= Hprime <= 1:
+                R1 = Chroma;
+                G1 = X;
+                B1 = 0;
+                break;
+            case 1 < Hprime <= 2:
+                R1 = X;
+                G1 = Chroma;
+                B1 = 0;
+                break;
+            case 2 < Hprime <= 3:
+                R1 = 0;
+                G1 = Chroma;
+                B1 = X;
+                break;
+            case 3 < Hprime <= 4:
+                R1 = 0;
+                G1 = X;
+                B1 = Chroma;
+            case 4 < Hprime <= 5:
+                R1 = X;
+                G1 = 0;
+                B1 = Chroma;
+                break;
+            case 5 < Hprime <= 6:
+                R1 = Chroma;
+                G1 = 0;
+                B1 = X;
+                break;
+        }
+
+        var m = V - Chroma;
+        var RealR = R1 + m;
+        var RealG = G1 + m;
+        var RealB = B1 + m;
+
+        var outputText = "Your color is R: " + RealR + ", G: " + RealG + ", B: " + RealB;
+        return handlerInput.responseBuilder.speak(speechText).getResponse();
+    }
+}
 const HelpIntentHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
